8ef59124c2455abc95c0e04e7696a2267a4b5a32
phuocthanhdo <phuocthanhdo@yahoo.com>
2022-07-28 11:13:14 +0700

    config.fish cfg

---
+++ config.fish
@@ -0,0 +1,14 @@
+abbr -a c cargo
+abbr -a cb cargo build
+abbr -a ct cargo test
+abbr -a e nvim
+abbr -a g git
+abbr -a gs git status
+abbr -a gc 'git commit -m'
+abbr -a ga 'git add -u'
+abbr -a gco 'git checkout'
+abbr -a gpu 'git pull upstream'
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
